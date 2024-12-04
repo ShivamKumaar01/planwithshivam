9fbdbf77342e4437c4d0fba8a27796631e07af63
ShivamKumaar01 <ShivamKumaar01@gmail.com>
2024-12-04 22:39:19 +0530

    initial commit

---
+++ src/components/Tours.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from './Card';
+
+const Tours = ({tours,removeTour}) => {
+  return (
+    <div className='container'>
+        <div>
+            <h2 className='title'>Plan With Shivam</h2>
+        </div>
+        <div className='cards'>
+        {
+            tours.map((tour)=>{
+                return <Card {...tour} removeTour={removeTour}></Card>
+                
+
+
+
+            })
+        }
+       
+
+        </div>
+    </div>
+    
+  );
+}
+
+export default Tours
